b620bb98c06621d88e8d10a58a5b3b79e347596b
George Vaughan <gvaughan9999@yahoo.com>
2023-05-08 17:51:53 -0400

    Adding initial versions of Shapes5.java and Rectangle5.java

---
+++ Rectangle5.java
@@ -0,0 +1,20 @@
+public class Rectangle5 {
+    private double length = 0;
+    private double width = 0;
+    private static int count = 0;
+
+    public Rectangle5(double length, 
+        double width) {
+        this.length = length;
+        this.width = width;
+        ++this.count;
+    }
+    
+    public double area() {
+        return(length * width);
+    }    
+    
+    public static int getCount() {
+        return count;
+    }
+}
---
+++ Shapes5.java
@@ -0,0 +1,20 @@
+public class Shapes5 {
+
+    public static void main(String[] args) {
+        System.out.println(Rectangle5.getCount() + 
+                " rectangles were created");
+        
+        Rectangle5 r1 = new Rectangle5(8.0, 5.0);
+        System.out.println("Area = " + r1.area());
+        
+        Rectangle5 r2 = new Rectangle5(3.0, 3.0);
+        System.out.println("Area = " + r2.area());
+        
+        Rectangle5 r3 = new Rectangle5(1.0, 7.0);
+        System.out.println("Area = " + r3.area());
+        
+        System.out.println(Rectangle5.getCount() + 
+                " rectangles were created");
+    }
+
+}
